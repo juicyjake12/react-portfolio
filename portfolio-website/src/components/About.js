d71e5b8ab7006d10a5b0dd8a8eb0ff1e4dd0bac5
juicyjake12 <jacob.hopper70@yahoo.com>
2023-01-09 20:49:47 -0500

    bio

--- portfolio-website/src/components/About.js
+++ portfolio-website/src/components/About.js
@@ -18,7 +18,9 @@ function About() {
 
 				<div className='BioParagraph'>
 					<p>
-						ABOUT Me
+						Hello my name is Jacob Hopper and i am a full stack developer. im from clio Mi and i currently work at menards in the outside yard and i 
+						am currently in pursait of a web development career. 
+						PS. NOT FINISHED WITH PORTFOLIO
 					</p>
 				</div>
 			</div>
